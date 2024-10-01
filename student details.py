472489e4d2f4ebb1cee6633387526dd4ca533c7c
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:23 +0530

    My first commit

---
+++ student details.py
@@ -0,0 +1,9 @@
+name=input('Enter the name of the student ')
+roll_number=int(input("Enter the Rollnumber"))
+roll_number=roll_number+1
+CGPA=float(input('Enter your CGPA'))
+Percentage=CGPA*10
+print('Name of the student:',name)
+print('Roll number of the student:',roll_number)
+print('CGPA of the student=',CGPA)
+print('Marks Percentage',Percentage,"%")
