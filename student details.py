358ad181324630ec120cf347442c88e0a91f2c6b
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-01 15:21:43 +0530

    Document Header Added

--- student details.py
+++ student details.py
@@ -1,3 +1,9 @@
+
+'''Python program to get the student details
+Author:Kevin P Tom
+Date:01-10-2024
+Version: 1.0'''
+
 name=input('Enter the name of the student ')
 roll_number=int(input("Enter the Rollnumber"))
 roll_number=roll_number+1
