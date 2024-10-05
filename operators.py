69fe23d63b01f3331ead46ed0340a91a7d273e20
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-05 16:06:25 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+a=10
+b=5
+#add two numbers
+sum=a+b
+#divion
+Division=a/b
+#print sum: and division
+print("Sum:",sum,",Division:",Division)
+#compare a and b using operand '>' and '=='
+print("Is a greater than b?:",a>b)
+print("Are a and b equal?:",a==b)
+#using Logical operators  AND and OR
+print("Logical AND:",a > b and a!=b)
+print("Logical OR:",a>b or a<b)
