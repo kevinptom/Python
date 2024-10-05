ea89578257cd0c8e6a3d23cdfe8d99d1a946f7ad
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-05 16:06:54 +0530

    math module

---
+++ math module.py
@@ -0,0 +1,5 @@
+import math
+enter_a_number=int(input("Enter a number:"))
+print("Square root of",enter_a_number,":",math.sqrt(enter_a_number))#finding suare root of input number
+print("Factorial of",enter_a_number,':',math.factorial(enter_a_number))#finding factorial of input number
+print(enter_a_number,'raised to power 2:',math.pow(enter_a_number,2))#finding the input number raised to 2
