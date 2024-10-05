7aa86124c512a2d4662a4fcb98de8141f207166d
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-05 16:14:23 +0530

    twinkle twinkle.py

--- twinkle twinkle.py
+++ twinkle twinkle.py
@@ -1,1 +1,6 @@
+'''Python program to perform math module
+Author:Kevin P Tom
+Date:05-10-2024
+Version: 1.0'''
+
 print( "Twinkle, twinkle, little star,\n\t\t How I wonder what you are!\n\t\t\t\t Up above the world so high,\n\t\t\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t\t How I wonder what you are")
