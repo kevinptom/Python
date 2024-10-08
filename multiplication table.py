6f9f12cade158c692b52cc13b12fe95f627c6b6d
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-08 14:54:13 +0530

    multiplication table.py

--- multiplication table.py
+++ multiplication table.py
@@ -1,3 +1,7 @@
+''Python program to print multiplication table of input number
+Author:Kevin P Tom
+Date:08-10-2024
+Version: 1.0'''
 number=int(input("Enter a number"))
 i=1
 #using for loop to print multiplication table
