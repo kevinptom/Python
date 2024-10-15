a5c5eac22d42f65dd75a75da3edf285880269e23
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-15 14:42:52 +0530

    smallest of three numbers

---
+++ smallest of three numbers.py
@@ -0,0 +1,12 @@
+''' Python program to find smallest of
+three numbers
+'''
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:"))
+n3=int(input("Enter the third number:"))
+if n1<n2 and n1<n2:
+    print(n1,"is the smallest number")
+elif n2<n3:
+    print(n2,"is the smallest number")
+else:
+    print(n3,"is the smallest number")
