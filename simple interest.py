2894baa1d3f555e94f57e4dab66d6267449cb4d2
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-15 14:32:53 +0530

    simple interest.py

--- simple interest.py
+++ simple interest.py
@@ -1,4 +1,7 @@
 ''' Python program to find simple interst
+Author:Kevin P Tom
+Date:08-10-2024
+Version: 1.0'
 '''
 p=int(input("Enter the principle amount:"))
 n=int(input("Enter the number of years(time period):"))
