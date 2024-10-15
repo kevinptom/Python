c4a0ec52afba5559e752a5057fd1ce4b88203204
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-15 14:18:10 +0530

    largest of two numbers

---
+++ largest of two numbers.py
@@ -0,0 +1,9 @@
+''' Python program to find the largest
+of two numbers
+'''
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+if num1>num2:
+    print(num1,"is the larger")
+else:
+    print(num2,"is the larger")
