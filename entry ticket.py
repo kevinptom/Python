aa065b05879c7967e8980078a8f253e2da3546c3
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-15 14:54:01 +0530

    entrt ticket

---
+++ entry ticket.py
@@ -0,0 +1,12 @@
+''' Python program to determine the entry ticket
+Author:Kevin P Tom
+Date:08-10-2024
+Version: 1.0
+'''
+age=int(input("Enter your age:"))
+if age<10:
+    print("Fare is 7")
+elif age>=10 and age<60:
+    print("Fare is 10")
+else:
+    print("Fare is 5")
