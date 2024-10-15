fe788cfa445704bdbb368e4157a009f04d3bdcd1
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-15 14:29:40 +0530

    simple interst

---
+++ simple interest.py
@@ -0,0 +1,7 @@
+''' Python program to find simple interst
+'''
+p=int(input("Enter the principle amount:"))
+n=int(input("Enter the number of years(time period):"))
+r=float(input("Enter a rate:"))
+simple_interst=p*n*r
+print(simple_interst)
