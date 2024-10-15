781db6d3948fe8301ba4ed185d28c7d84c7a3bb1
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-15 16:30:34 +0530

    sum of digit

---
+++ sum of digit.py
@@ -0,0 +1,12 @@
+''' Python program to find first n odd numbers
+Author:Kevin P Tom
+Date:15-10-2024
+Version: 1.0
+'''
+num=int(input("Enter a number:"))
+sum=0
+while(num>0):
+    r=num%10
+    num=num//10
+    sum=sum+r
+print(sum)
