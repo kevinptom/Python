050caf76740e7f922b0961ba6a59b9a84deb037f
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-17 10:09:57 +0530

    odd or even.py

--- odd or even.py
+++ odd or even.py
@@ -1,3 +1,7 @@
+'''Python program to print multiplication table of input number
+Author:Kevin P Tom
+Date:05-10-2024
+Version: 1.0'''
 enter_a_number=int(input("Enter a number"))
 if enter_a_number%2==0:
     print(enter_a_number,"is even")
