a7301c7ff3fe934539427466605c26c1b368eb45
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-28 17:01:20 +0530

    leap year.py

--- leap year.py
+++ leap year.py
@@ -1,4 +1,7 @@
-
+'''Python program to check whether the input year is Leap year or not
+Author:Kevin P Tom
+Date:28-10-2024
+Version: 1.0'''
 year=int(input("Enter a year "))
 if year%4==0:
 	print("Leap year")
