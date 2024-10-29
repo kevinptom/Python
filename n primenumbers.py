01e860b64a057065d6c9e1d0ceea85c08bf340df
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-10-29 15:02:44 +0530

    Add files via upload

---
+++ n primenumbers.py
@@ -0,0 +1,10 @@
+limit=int(input("Enter a number:"))
+for number in range(2,limit+1):
+    isPrime=True
+    for i in range(2,number//2+1):
+        if number% i ==0:
+            isPrime=False
+            break
+    if isPrime==True:
+        print(number,end=" ")
+
