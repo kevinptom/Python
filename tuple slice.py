ee17296effd04abd92dc42beb11a56a13c9f02fd
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-19 13:41:56 +0530

    Add files via upload

---
+++ tuple slice.py
@@ -0,0 +1,11 @@
+colors = ('red','green','blue','yellow','orange')
+tup1 = colors[0:3]
+print(tup1)
+tup2 = colors[-2:]
+print(tup2)
+
+
+person = ('Alice',25,'Engineer')
+#Unpack into variables
+name,age,job = person
+print(name)
